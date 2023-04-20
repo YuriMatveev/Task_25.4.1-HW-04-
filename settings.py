5bf5202f68642186bebc32338a237fd45cfa8b13
YuriMatveev <110047075+YuriMatveev@users.noreply.github.com>
2023-04-20 08:25:12 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,10 @@
+valid_email = 'johnny_walker@gmail.com'
+valid_password = '123_password'
+
+not_valid_email = 'johnny@mail.ru'
+not_valid_password = 'password_123'
+
+vacant_email = ''
+vacant_password = ''
+
+empty_auth_key = {'key': ''}
